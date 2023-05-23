71c6ee7144d0714201181d311d53990017f8ac15
su11mooo <108126509+su11mooo@users.noreply.github.com>
2023-05-23 15:09:52 +0900

    二回目

---
+++ src/Assets/Scripts/PuyoController.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PuyoType
+{
+    Black=0,
+
+    Green=1,
+    red=2,
+    Yellow=3,
+    Blue=4,
+    purple=5,
+    Cyan=6,
+
+    Invalid=7,
+
+};
+
+[RequireComponent(typeof(Renderer))]
+public class PuyoController : MonoBehaviour
+{
+    static readonly Color[] colors_table = new Color[]
+        {
+        Color.black,
+
+        Color.green,
+        Color.red,
+        Color.yellow,
+        Color.blue,
+        Color.magenta,
+        Color.cyan,
+
+        Color.gray,
+        };
+
+    [SerializeField] Renderer my_renderer = default!;
+    PuyoType _type= PuyoType.Invalid;
+
+    public void SetPuyoType(PuyoType type)
+    {
+        _type = type;
+        my_renderer.material.color= colors_table[(int)_type];
+    }
+
+    public PuyoType GetPuyoType()
+    {
+        return _type;
+    }
+
+    public void SetPos(Vector3 pos)
+    {
+        this.transform.localPosition = pos;
+    }
+}
