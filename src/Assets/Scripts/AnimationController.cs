387687e2f50430141aaba1aee841acd8463677f4
su11mooo <108126509+su11mooo@users.noreply.github.com>
2023-07-24 18:38:14 +0900

    webgl build
    Create activation.yml
    
    Delete .github/workflows directory
    
    Create activation.yml
    
    Delete .github/workflows directory
    
    Create activation.yml
    
    Create test.yml
    
    Create webgl_build.yml
    
    remove zip file

---
+++ src/Assets/Scripts/AnimationController.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationController 
+{
+    const float DELTA_TIME_MAX = 1.0f;
+    float _time = 0.0f;
+    float _inv_time_max = 1.0f;
+
+    public void Set(float max_time)
+    {
+        Debug.Assert(0.0f<max_time);
+
+        _time = max_time;
+        _inv_time_max = 1.0f / max_time;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public bool Update(float delta_time)
+    {
+        if (DELTA_TIME_MAX < delta_time)delta_time= DELTA_TIME_MAX;
+        _time -= delta_time;
+
+        if (_time < 0.0f)
+        {
+            _time = 0.0f;
+            return false;
+
+        }
+        
+        return true;
+    }
+
+    public float GetNormalized()
+    {
+        return _time * _inv_time_max;
+    }
+
+}
